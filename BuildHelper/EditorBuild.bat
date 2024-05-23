c632c1d0c77ff96ca86420338306cb34fae814d6
wkdtpeo <wkdtpeo@naver.com>
2024-05-24 00:55:34 +0900

    [UnrealEngine GitDependencies Test]

--- BuildHelper/EditorBuild.bat
+++ BuildHelper/EditorBuild.bat
@@ -6,3 +6,32 @@ echo EditorBuild
 echo =========================================
 echo Run %~nx0
 echo Current Directory: %CD%
+
+echo =========================================
+echo Check UnrealEngine GitDependencies...
+echo =========================================
+setlocal
+pushd %~dp0
+
+@rem Sync the dependencies...
+.\Engine\Binaries\DotNET\GitDependencies\win-x64\GitDependencies.exe %*
+if ERRORLEVEL 1 goto error
+
+@rem Setup the git hooks...
+if not exist .git\hooks goto no_git_hooks_directory
+echo Registering git hooks...
+echo #!/bin/sh >.git\hooks\post-checkout
+echo Engine/Binaries/DotNET/GitDependencies/win-x64/GitDependencies.exe %* >>.git\hooks\post-checkout
+echo #!/bin/sh >.git\hooks\post-merge
+echo Engine/Binaries/DotNET/GitDependencies/win-x64/GitDependencies.exe %* >>.git\hooks\post-merge
+:no_git_hooks_directory
+
+@rem Done!
+goto DONE
+
+@rem Error happened. Wait for a keypress before quitting.
+:error
+echo %~nx0 Failed GitDependencies...
+
+:DONE
+popd
