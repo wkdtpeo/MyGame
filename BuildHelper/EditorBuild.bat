3f1cb483ebbebde63941a2577cd960fb8cd9f456
wkdtpeo <wkdtpeo@naver.com>
2024-05-25 01:17:58 +0900

    EditorBuild.bat 수정

--- BuildHelper/EditorBuild.bat
+++ BuildHelper/EditorBuild.bat
@@ -1,12 +1,16 @@
 @echo off
 @rem UE5 Editor Build
+setlocal
+pushd "%~dp0"
 
+echo.
 echo =========================================
 echo Run EditorBuild.bat
 echo =========================================
 echo Run %~nx0
 echo Current Directory: %CD%
 
+echo.
 echo =========================================
 echo Unreal Engine Setup...
 echo =========================================
@@ -22,6 +26,7 @@ echo ERRORLEVEL is %ERRORLEVEL%
 goto DONE
 
 :DONE
-echo -------- EditorBuild.bat Done. -------- 
+echo -------- EditorBuild.bat Done! -------- 
 popd
-
+endlocal
+echo.
