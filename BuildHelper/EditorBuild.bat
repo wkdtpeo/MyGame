59b708e339f2ca47f79a935ae9397c0980c4db20
wkdtpeo <wkdtpeo@naver.com>
2024-05-25 01:11:30 +0900

    GitDependecy 설치를 언리얼에서 기본으로 제공하는 Setup.bat으로 실행하도록 변경 (테스트)

--- BuildHelper/EditorBuild.bat
+++ BuildHelper/EditorBuild.bat
@@ -2,36 +2,26 @@
 @rem UE5 Editor Build
 
 echo =========================================
-echo EditorBuild
+echo Run EditorBuild.bat
 echo =========================================
 echo Run %~nx0
 echo Current Directory: %CD%
 
 echo =========================================
-echo Check UnrealEngine GitDependencies...
+echo Unreal Engine Setup...
 echo =========================================
-setlocal
-pushd %~dp0
-
-@rem Sync the dependencies...
-..\Engine\Binaries\DotNET\GitDependencies\win-x64\GitDependencies.exe %*
-if ERRORLEVEL 1 goto error
-
-@rem Setup the git hooks...
-if not exist .git\hooks goto no_git_hooks_directory
-echo Registering git hooks...
-echo #!/bin/sh >.git\hooks\post-checkout
-echo Engine/Binaries/DotNET/GitDependencies/win-x64/GitDependencies.exe %* >>.git\hooks\post-checkout
-echo #!/bin/sh >.git\hooks\post-merge
-echo Engine/Binaries/DotNET/GitDependencies/win-x64/GitDependencies.exe %* >>.git\hooks\post-merge
-:no_git_hooks_directory
+call ..\Setup.bat
+if %ERRORLEVEL% NEQ 0 goto FAILED
 
 @rem Done!
 goto DONE
 
-@rem Error happened. Wait for a keypress before quitting.
-:error
-echo %~nx0 Failed GitDependencies...
+:FAILED
+echo -------- EditorBuild.bat Failed!! -------- 
+echo ERRORLEVEL is %ERRORLEVEL%
+goto DONE
 
 :DONE
+echo -------- EditorBuild.bat Done. -------- 
 popd
+
